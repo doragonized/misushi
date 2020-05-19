a736495c9f98b8fa7a6e59cf7e51939e62e6dafa
doragonized <anyixu98@hotmail.com>
2020-05-19 14:45:02 +0200

    1st

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  return {
+    dir: { input: 'src', output: 'dist', data: '_data' },
+    passthroughFileCopy: true,
+    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+    htmlTemplateEngine: 'njk'
+  }
+}
